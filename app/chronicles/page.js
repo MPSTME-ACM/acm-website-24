1cfb3fc38b01823cc9ec33854bdf5016b1679afd
Yash <78804783+yashd-dev@users.noreply.github.com>
2025-01-06 18:45:33 +0530

    build fix

--- app/chronicles/page.js
+++ app/chronicles/page.js
@@ -1,10 +1,14 @@
 "use client";
 import PDFFlipbook from "@/components/FlipBok";
+import { Suspense } from "react";
 
 export default function ChroniclesPage() {
   return (
     <section className="flex flex-col md:flex-row w-screen items-center  justify-center  overflow-x-hidden text-brand-surface py-20 md:pt-40 max-w-[90rem] mx-auto px-8 lg:px-0 gap-10 ">
-      <PDFFlipbook pdfUrl="/newsletter.pdf" />
+      <Suspense fallback={<div>Loading...</div>}>
+            <PDFFlipbook pdfUrl="/newsletter.pdf" />
+
+      </Suspense>
     </section>
   );
 }
