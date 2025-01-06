e2a5634db2188d6670bf5d037f7a94244a0d1ff1
Yash D <78804783+yashd-dev@users.noreply.github.com>
2025-01-06 18:02:28 +0530

    Remove Localhost

--- app/chronicles/page.js
+++ app/chronicles/page.js
@@ -4,7 +4,7 @@ import PDFFlipbook from "@/components/FlipBok";
 export default function ChroniclesPage() {
   return (
     <section className="flex flex-col md:flex-row w-screen items-center  justify-center  overflow-x-hidden text-brand-surface py-20 md:pt-40 max-w-[90rem] mx-auto px-8 lg:px-0 gap-10 ">
-      <PDFFlipbook pdfUrl="http://localhost:3000/newsletter.pdf" />
+      <PDFFlipbook pdfUrl="/newsletter.pdf" />
     </section>
   );
 }
