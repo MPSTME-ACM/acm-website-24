80b674a673b823ec7ae2ff8b091c9a8ab5d25f19
Yash D <78804783+yashd-dev@users.noreply.github.com>
2025-01-28 23:48:41 +0530

    feat: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["cdn.sanity.io"],
-    },
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/semicode',
+        destination: 'https://unstop.com/hackathons/acm-semicode-mukesh-patel-school-of-technology-management-and-engineering-nmims-mumbai-1365088',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
